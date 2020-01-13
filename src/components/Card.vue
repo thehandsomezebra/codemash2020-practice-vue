6d30a634789b407b11b347687d87653b882ec1bc
stephanie <stephlage@gmail.com>
2020-01-12 21:28:56 -0500

    workshop during codemash 2020 (jan 6-10)

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="['card', 'img-' + imgDirection, 'rounded-0']">
+    <img :src="imgSrc" class="card-img rounded-0" :alt="imgAlt" />
+    <div class="card-body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["imgDirection", "imgSrc", "imgAlt"]
+};
+</script>
+
+<style lang="scss">
+.card {
+  align-items: flex-start;
+
+  &.img-left {
+    flex-direction: row;
+
+    .card-img {
+      width: 33%;
+    }
+  }
+
+  &.img-right {
+    flex-direction: row-reverse;
+
+    .card-img {
+      width: 33%;
+    }
+  }
+
+  &.img-bottom {
+    flex-direction: column-reverse;
+  }
+}
+</style>
